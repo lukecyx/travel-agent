db3a98eff2338ec096ef9c0c6f77af6133845cd0
Luke Holmes <lukemartynholmes@gmail.com>
2025-03-03 19:35:14 +0000

    feat(ui): Add text area autogrow.

--- actions/sendMessage.ts
+++ actions/sendMessage.ts
@@ -1,26 +1,7 @@
 "use server";
 
 import { runAgent } from "@/lib/cs-agent/src/agent";
-import { z } from "zod";
 
-export async function sendMessageAction(formData: unknown) {
-  if (!(formData instanceof FormData)) {
-    throw new Error("no form data");
-  }
-
-  const userMessageSchema = z.object({
-    userMessage: z.string(),
-  });
-
-  const result = userMessageSchema.safeParse(Object.fromEntries(formData));
-  if (!result.success) {
-    console.error(result.error.format());
-    return;
-  } else {
-    console.log("✅", result.data);
-  }
-
-  const ret = await runAgent({ userMessage: result.data?.userMessage });
-
-  return ret;
+export async function sendMessageAction(userMessage: string) {
+  return await runAgent({ userMessage: userMessage });
 }
